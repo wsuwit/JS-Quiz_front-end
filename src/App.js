ae60f98a391e93df71768a4f5f531ec5b8c30b9c
wsuwit <w_suwit-456@hotmail.com>
2021-10-13 02:13:50 +0700

    Save LeaderBoard feature

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
 import About from "./components/About";
 import Home from "./components/Home";
+import LeaderBoard from "./components/LeaderBoard";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="w3-container">
-      {/* <Navbar /> */}
+      <Navbar />
       {/* <Home /> */}
-      <About />
+      {/* <About /> */}
+      <LeaderBoard />
     </div>
   );
 }
