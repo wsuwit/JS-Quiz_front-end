89cd30fee21c5ab4148a27dbd0cf7ce764909f91
wsuwit <w_suwit-456@hotmail.com>
2021-10-13 02:08:35 +0700

    Save About feature

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
+import About from "./components/About";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="w3-container">
-      <Navbar />
-      <Home />
+      {/* <Navbar /> */}
+      {/* <Home /> */}
+      <About />
     </div>
   );
 }
